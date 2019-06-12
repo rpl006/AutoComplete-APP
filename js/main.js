8dc96084b68191ce229fe75bd76cf29fee48220b
Ram Lamichhane <ram.lamichhane@aol.com>
2019-06-12 11:43:09 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const matchList = document.getElementById("match-list");
 //Search states.json and filter it
 
 const searchStates = async searchText => {
-  const res = await fetch("../data/states.json");
+  const res = await fetch("./data/states.json");
   const states = await res.json();
 
   //Get Matches to current text input
